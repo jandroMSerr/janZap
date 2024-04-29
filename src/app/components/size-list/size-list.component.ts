62cf8c4561e50cb7e3dc73b85ebf7c19574c8ac7
TFG Jandro <tfgecomercejandro@gmail.com>
2024-04-29 17:16:25 +0200

    adding changes

---
+++ src/app/components/size-list/size-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-size-list',
+  templateUrl: './size-list.component.html',
+  styleUrls: ['./size-list.component.scss']
+})
+export class SizeListComponent {
+
+}
