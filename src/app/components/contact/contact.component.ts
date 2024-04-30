b9a7e9f9b15d3eecca5b35a7964793fe0e2732d4
TFG Jandro <tfgecomercejandro@gmail.com>
2024-04-30 16:45:39 +0200

    adding navbar and footer

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  formData = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+  };
 
+  submitForm() {
+    if (!this.formData.nombre || !this.formData.email || !this.formData.mensaje) {
+      alert('Por favor, rellena todos los campos.');
+      return;
+    }
+    
+    // Aquí puedes agregar la lógica para enviar el formulario, como enviarlo a través de un servicio HTTP
+    alert('El formulario se ha enviado.');
+    // Puedes agregar lógica adicional aquí, como restablecer el formulario o navegar a otra página
+  }
+  
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import
+{ Router }
+from'@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+ 
 }
